eff87e639d1317d937aedb0a9360f638b5ef22ad
Steelcap <Darkblade.1@gmail.com>
2016-09-22 10:56:09 -0700

    Ghoul's Advice

--- NSLCompMod/output/lua/DPR/BalanceHealth.lua
+++
@@ -1,4 +0,0 @@
-kCystHealth = 30    kCystArmor = 0
-kMatureCystHealth = 180    kMatureCystArmor = 0    kCystPointValue = 1
-kMinMatureCystHealth = 180 kMinCystScalingDistance = 1 kMaxCystScalingDistance = 1
-kLerkArmor = 55 kLerkArmorFullyUpgradedAmount = 70

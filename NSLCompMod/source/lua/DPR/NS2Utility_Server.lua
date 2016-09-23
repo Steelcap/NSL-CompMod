9767ae62e47b74e736fb49e0f4d4db26dfa7704b
Steelcap <Darkblade.1@gmail.com>
2016-09-23 08:55:47 -0700

    Removed Abilities on Hive Death
    No longer preserve abilities that aliens had while alive if biomass
    falls under minimum required level.

---
+++ NSLCompMod/source/lua/DPR/NS2Utility_Server.lua
@@ -0,0 +1,114 @@
+Script.Load("lua/NS2Utility_Server.lua")
+Script.Load("lua/Table.lua")
+Script.Load("lua/Utility.lua")
+
+local function UnlockAbility(forAlien, techId)
+
+    local mapName = LookupTechData(techId, kTechDataMapName)
+    if mapName and forAlien:GetIsAlive() then
+    
+        local activeWeapon = forAlien:GetActiveWeapon()
+
+        local tierWeapon = forAlien:GetWeapon(mapName)
+        if not tierWeapon then
+        
+            forAlien:GiveItem(mapName)
+            
+            if activeWeapon then
+                forAlien:SetActiveWeapon(activeWeapon:GetMapName())
+            end
+            
+        end
+    
+    end
+
+end
+
+local function LockAbility(forAlien, techId)
+
+    local mapName = LookupTechData(techId, kTechDataMapName)    
+    if mapName and forAlien:GetIsAlive() then
+    
+        local tierWeapon = forAlien:GetWeapon(mapName)
+        local activeWeapon = forAlien:GetActiveWeapon()
+        local activeWeaponMapName = nil
+        
+        if activeWeapon ~= nil then
+            activeWeaponMapName = activeWeapon:GetMapName()
+        end
+        
+        if tierWeapon then
+            forAlien:RemoveWeapon(tierWeapon)
+        end
+        
+        if activeWeaponMapName == mapName then
+            forAlien:SwitchWeapon(1)
+        end
+        
+    end    
+    
+end
+
+local function CheckHasPrereq(teamNumber, techId)
+
+    local hasPrereq = false
+
+    local techTree = GetTechTree(teamNumber)
+    if techTree then
+        
+        local techNode = techTree:GetTechNode(techId)
+        if techNode then
+            hasPrereq = techTree:GetHasTech(techNode:GetPrereq1())
+        end
+    
+    end
+
+    return hasPrereq
+
+end
+
+function UpdateAbilityAvailability(forAlien, tierOneTechId, tierTwoTechId, tierThreeTechId)
+
+    local time = Shared.GetTime()
+    if forAlien.timeOfLastNumHivesUpdate == nil or (time > forAlien.timeOfLastNumHivesUpdate + 0.5) then
+
+        local team = forAlien:GetTeam()
+        if team and team.GetTechTree then
+        
+            local hasOneHiveNow = GetGamerules():GetAllTech() or (tierOneTechId ~= nil and tierOneTechId ~= kTechId.None and GetIsTechUnlocked(forAlien, tierOneTechId))
+            -- lose abilities
+            forAlien.oneHive = hasOneHiveNow
+
+            if forAlien.oneHive then
+                UnlockAbility(forAlien, tierOneTechId)
+            else
+                LockAbility(forAlien, tierOneTechId)
+            end
+            
+            local hasTwoHivesNow = GetGamerules():GetAllTech() or (tierTwoTechId ~= nil and tierTwoTechId ~= kTechId.None and GetIsTechUnlocked(forAlien, tierTwoTechId))
+			-- lose abilities
+            forAlien.twoHives =  hasTwoHivesNow
+
+            if forAlien.twoHives then
+                UnlockAbility(forAlien, tierTwoTechId)
+            else
+                LockAbility(forAlien, tierTwoTechId)
+            end
+            
+            local hasThreeHivesNow = GetGamerules():GetAllTech() or (tierThreeTechId ~= nil and tierThreeTechId ~= kTechId.None and GetIsTechUnlocked(forAlien, tierThreeTechId))
+            -- lose abilities
+            forAlien.threeHives = hasThreeHivesNow
+
+            if forAlien.threeHives then
+                UnlockAbility(forAlien, tierThreeTechId)
+            else
+                LockAbility(forAlien, tierThreeTechId)
+            end
+            
+        end
+        
+        forAlien.timeOfLastNumHivesUpdate = time
+        
+    end
+
+end

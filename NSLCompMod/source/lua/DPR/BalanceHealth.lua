b676cea9ee6903279c1256ae7de87a777c1acd3a
keatsandyeats <keatsandyeats@users.noreply.github.com>
2016-09-24 19:36:35 -0400

    increase mine ehp to 50

--- NSLCompMod/source/lua/DPR/BalanceHealth.lua
+++ NSLCompMod/source/lua/DPR/BalanceHealth.lua
@@ -2,3 +2,4 @@ kCystHealth = 30    kCystArmor = 0
 kMatureCystHealth = 180    kMatureCystArmor = 0    kCystPointValue = 1
 kMinMatureCystHealth = 180 kMinCystScalingDistance = 1 kMaxCystScalingDistance = 1
 kLerkArmor = 55 kLerkArmorFullyUpgradedAmount = 70
+kMineHealth = 40 kMineArmor = 5

f9dc502b3931adfc983c80bc9a2cd7484dd5dd05
Steelcap <Darkblade.1@gmail.com>
2016-10-17 19:45:07 -0700

    Integrate Vanilla Lerk Bring 310 lerk changes into compmod

--- NSLCompMod/source/lua/DPR/BalanceHealth.lua
+++ NSLCompMod/source/lua/DPR/BalanceHealth.lua
@@ -1,5 +1,5 @@
 kCystHealth = 30    kCystArmor = 0
 kMatureCystHealth = 180    kMatureCystArmor = 0    kCystPointValue = 1
 kMinMatureCystHealth = 180 kMinCystScalingDistance = 1 kMaxCystScalingDistance = 1
-kLerkArmor = 55 kLerkArmorFullyUpgradedAmount = 70
+kLerkArmor = 45 kLerkArmorFullyUpgradedAmount = 60
 kMineHealth = 40 kMineArmor = 5

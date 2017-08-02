3aec634d4c3890f4fab69c017d4c448e8dc15b1c
keatsandyeats <keatsandyeats@users.noreply.github.com>
2017-08-02 18:36:05 -0400

    Reorganize folder structure
    Everything is currently a filehook, so let's make hooking more
    automatic.

---
+++ NSLCompMod/source/lua/DPR_FileHooks.lua
@@ -0,0 +1,23 @@
+--This global disables balance changes of the UWE Extensions
+gDisableUWEBalance = true
+
+local function GetVanillaPath(DPRPath)
+    DPRPath = string.gsub(DPRPath, "DPR/Replace/", "")
+    DPRPath = string.gsub(DPRPath, "DPR/Post/", "")
+    
+    return DPRPath
+end
+
+local ReplaceFiles = {}
+Shared.GetMatchingFileNames("lua/DPR/Replace/*.lua", true, ReplaceFiles) --what does the bool do? recurse?
+
+for i = 1, #ReplaceFiles do
+    ModLoader.SetupFileHook(GetVanillaPath(ReplaceFiles[i]), ReplaceFiles[i], "replace")
+end
+
+local PostFiles = {}
+Shared.GetMatchingFileNames("lua/DPR/Post/*.lua", true, PostFiles) --what does the bool do? recurse?
+
+for i = 1, #PostFiles do
+    ModLoader.SetupFileHook(GetVanillaPath(PostFiles[i]), PostFiles[i], "post")
+end

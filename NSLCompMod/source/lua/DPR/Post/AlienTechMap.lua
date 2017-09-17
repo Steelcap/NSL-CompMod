6b584143649b56b0907176dda488ff1bbff09b9e
keatsandyeats <keatsandyeats@users.noreply.github.com>
2017-09-16 22:04:20 -0400

    Make updated lines for the Alien TechMap
    We change some tech around so some lines need to change too.
    Changing them all is easier than programmatically generating a
    new map in a way that handles any possible changes.

--- NSLCompMod/source/lua/DPR/Post/AlienTechMap.lua
+++ NSLCompMod/source/lua/DPR/Post/AlienTechMap.lua
@@ -19,3 +19,36 @@ for techIndex = #kAlienTechMap, 1, -1 do
         kAlienTechMap[techIndex] = techToMove[techId]
     end
 end
+
+-- remove focus
+-- move silence to veils
+kAlienLines = {
+    GetLinePositionForTechMap(kAlienTechMap, kTechId.Hive, kTechId.Crag),
+    GetLinePositionForTechMap(kAlienTechMap, kTechId.Hive, kTechId.Shift),
+    GetLinePositionForTechMap(kAlienTechMap, kTechId.Hive, kTechId.Shade),
+    GetLinePositionForTechMap(kAlienTechMap, kTechId.Hive, kTechId.Whip),
+
+    GetLinePositionForTechMap(kAlienTechMap, kTechId.Harvester, kTechId.Hive),
+    GetLinePositionForTechMap(kAlienTechMap, kTechId.Hive, kTechId.Drifter),
+
+    GetLinePositionForTechMap(kAlienTechMap, kTechId.Hive, kTechId.ShadeHive),
+    {4, 2.5, 10, 2.5}, --just above crag hive to just above shift hive
+    {4, 2.5, 4, 3}, --just above crag hive to crag hive
+    {10, 2.5, 10, 3}, --just above shift hive to shift hive
+
+    GetLinePositionForTechMap(kAlienTechMap, kTechId.CragHive, kTechId.Shell),
+    GetLinePositionForTechMap(kAlienTechMap, kTechId.ShadeHive, kTechId.Veil),
+    GetLinePositionForTechMap(kAlienTechMap, kTechId.ShiftHive, kTechId.Spur),
+
+    GetLinePositionForTechMap(kAlienTechMap, kTechId.Shell, kTechId.Crush),
+    GetLinePositionForTechMap(kAlienTechMap, kTechId.Shell, kTechId.Carapace),
+    GetLinePositionForTechMap(kAlienTechMap, kTechId.Shell, kTechId.Regeneration),
+
+    -- move silence to veil
+    GetLinePositionForTechMap(kAlienTechMap, kTechId.Veil, kTechId.Silence),
+    GetLinePositionForTechMap(kAlienTechMap, kTechId.Veil, kTechId.Vampirism),
+    GetLinePositionForTechMap(kAlienTechMap, kTechId.Veil, kTechId.Aura),
+
+    GetLinePositionForTechMap(kAlienTechMap, kTechId.Spur, kTechId.Celerity),
+    GetLinePositionForTechMap(kAlienTechMap, kTechId.Spur, kTechId.Adrenaline),
+}

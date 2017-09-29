bcdde1e052de47dbb3c1f19554b29f3c9496b1cc
keatsandyeats <keatsandyeats@users.noreply.github.com>
2017-09-28 22:26:25 -0400

    Cluster grenades burn and destroy alien abilities
    Just like flamethrower, cluster grenades burn the following:
    spores, umbra, bilebomb (from gorge and contamination), whip bomb,
    mucous, enzyme

--- NSLCompMod/source/lua/DPR/Post/Weapons/Marine/ClusterGrenade.lua
+++ NSLCompMod/source/lua/DPR/Post/Weapons/Marine/ClusterGrenade.lua
@@ -1,6 +1,7 @@
--- Cluster Grenades set structures and players on fire
--- Thanks Dragon :)
+-- cluster grenades set stuff on fire
 if Server then
+    -- Cluster Grenades set structures and players on fire
+    -- Thanks Dragon :)
     local function IgniteNearbyEntities(self, range)
         local hitEntities = GetEntitiesWithMixinWithinRange("Fire", self:GetOrigin(), range)
         local player = self:GetOwner()
@@ -12,9 +13,44 @@ if Server then
         end
     end
 
+    -- Cluster Grenades burn and destroy abilities
+    local function BurnNearbyAbilities(self, range)
+        --TODO: should range be cluster size or individual cloud size?
+        local grenadePos = self:GetOrigin()
+
+        -- lerk spores
+        local spores = GetEntitiesWithinRange("SporeCloud", grenadePos, range)
+
+        -- lerk umbra, mucous, enzyme
+        local umbras = GetEntitiesWithinRange("CragUmbra", grenadePos, range)
+        table.copy(GetEntitiesWithinRange("MucousMembrane", grenadePos, range), umbras, true)
+        table.copy(GetEntitiesWithinRange("EnzymeCloud", grenadePos, range), umbras, true)
+
+        -- bilebomb (gorge and contamination), whip bomb
+        local bombs = GetEntitiesWithinRange("Bomb", grenadePos, range)
+        table.copy(GetEntitiesWithinRange("WhipBomb", grenadePos, range), bombs, true)
+
+        for _, spore in ipairs(spores) do
+            self:TriggerEffects("burn_spore", {effecthostcoords = Coords.GetTranslation(spore:GetOrigin())})
+            DestroyEntity(spore)
+        end
+
+        for _, umbra in ipairs(umbras) do
+            self:TriggerEffects("burn_umbra", {effecthostcoords = Coords.GetTranslation(umbra:GetOrigin())})
+            DestroyEntity(umbra)
+        end
+
+        for _, bomb in ipairs(bombs) do
+            self:TriggerEffects("burn_bomb", {effecthostcoords = Coords.GetTranslation(bomb:GetOrigin())})
+            DestroyEntity(bomb)
+        end
+    end
+
     local oldClusterGrenadeDetonate = ClusterGrenade.Detonate
     function ClusterGrenade:Detonate(targetHit)
         IgniteNearbyEntities(self, kClusterGrenadeDamageRadius)
+        BurnNearbyAbilities(self, kClusterGrenadeDamageRadius)
+
         oldClusterGrenadeDetonate(self, targetHit)
     end
 end

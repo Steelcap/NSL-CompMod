5611313d2e4db589524c873c65e7311b51a8e7f9
Steelcap <Darkblade.1@gmail.com>
2018-02-19 16:36:06 -0800

    Update onos knockback for marines, null for exo

--- NSLCompMod/source/lua/DPR/Post/Onos.lua
+++ NSLCompMod/source/lua/DPR/Post/Onos.lua
@@ -2,10 +2,10 @@ Onos.kMaxSpeed = 7.5
 
 Onos.kStampedeDefaultSettings =
 {
-    kChargeImpactForce = 0,
-    kChargeDiffForce = 0,
-    kChargeUpForce = 0,
-    kDisableDuration = 0.2,
+    kChargeImpactForce = 0.6,
+    kChargeDiffForce = 5,
+    kChargeUpForce = 1,
+    kDisableDuration = 0.05,
 }
 
 -- Mods can add their own overrides for other classes.

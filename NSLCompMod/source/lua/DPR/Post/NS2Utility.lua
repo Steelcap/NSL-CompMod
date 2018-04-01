5dbe9ba40ed143b7e40fedb462995be688d91673
Steelcap <Darkblade.1@gmail.com>
2018-04-01 12:21:50 -0700

    Remove Mucus and FT FF

--- NSLCompMod/source/lua/DPR/Post/NS2Utility.lua
+++ NSLCompMod/source/lua/DPR/Post/NS2Utility.lua
@@ -26,3 +26,78 @@ function GetMaxSupplyForTeam(teamNumber)
 
     return maxSupply 
 end
+
+
+
+-- All damage is routed through here.
+function CanEntityDoDamageTo(attacker, target, cheats, devMode, friendlyFire, damageType)
+
+    if not GetGameInfoEntity():GetGameStarted() and not GetGameInfoEntity():GetWarmUpActive() then
+        return false
+    end
+
+    if target:isa("Clog") then
+        return true
+    end
+
+    if not HasMixin(target, "Live") then
+        return false
+    end
+
+    if GetGameInfoEntity():GetWarmUpActive() and not GetValidTargetInWarmUp(target) then
+        return false
+    end
+
+    if target:isa("ARC") and damageType == kDamageType.Splash then
+        return true
+    end
+
+    if not target:GetCanTakeDamage() then
+        return false
+    end
+
+    if target == nil or (target.GetDarwinMode and target:GetDarwinMode()) then
+        return false
+    elseif cheats or devMode then
+        return true
+    elseif attacker == nil then
+        return true
+    end
+
+    -- You can always do damage to yourself.
+    if attacker == target then
+        return true
+    end
+
+    -- Command stations can kill even friendlies trapped inside.
+    if attacker ~= nil and attacker:isa("CommandStation") then
+        return true
+    end
+
+    -- Your own grenades can hurt you.
+    if attacker:isa("Grenade") then
+
+        local owner = attacker:GetOwner()
+        if owner and owner:GetId() == target:GetId() then
+            return true
+        end
+
+    end
+
+    -- Same teams not allowed to hurt each other unless friendly fire enabled.
+    local teamsOK = true
+    if attacker ~= nil then
+        teamsOK = GetAreEnemies(attacker, target) or friendlyFire
+    end
+
+	-- COMPMOD Prevent FT puddles from hurting teammates
+	if attacker:isa("Flame") then
+		if not GetAreEnemies(attacker, target) then
+			return false
+		end
+	end
+	
+    -- Allow damage of own stuff when testing.
+    return teamsOK
+
+end

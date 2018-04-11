d79e6e56a27049e288d260ebcab3a8fcb9a976b8
Steelcap <Darkblade.1@gmail.com>
2018-04-11 11:45:46 -0700

    Updated tooltips

--- NSLCompMod/source/lua/DPR/Shared/TechData.lua
+++ NSLCompMod/source/lua/DPR/Shared/TechData.lua
@@ -33,8 +33,8 @@ function BuildTechData()
     local techData = oldBuildTechData()
     NSLTechDataChanges(techData)
 		
-	local medtech1 = { [kTechDataId] = kTechId.MedTech1,       [kTechDataDisplayName] = "Medpack Tech 1", [kTechDataTooltipInfo] = "Improves Medpacks", [kTechDataCostKey] = kTechMed1ResearchCost,             [kTechDataResearchTimeKey] = kTechMed1ResearchTime, }
-	local medtech2 = { [kTechDataId] = kTechId.MedTech2,       [kTechDataDisplayName] = "Medpack Tech 2", [kTechDataTooltipInfo] = "Improves Medpacks", [kTechDataCostKey] = kTechMed2ResearchCost,             [kTechDataResearchTimeKey] = kTechMed2ResearchTime, }
+	local medtech1 = { [kTechDataId] = kTechId.MedTech1,       [kTechDataDisplayName] = "Medpack Upgrade #1", [kTechDataTooltipInfo] = "Improves Medpacks to 30 instant heal and 20 regen", [kTechDataCostKey] = kTechMed1ResearchCost,             [kTechDataResearchTimeKey] = kTechMed1ResearchTime, }
+	local medtech2 = { [kTechDataId] = kTechId.MedTech2,       [kTechDataDisplayName] = "Medpack Upgrade #2", [kTechDataTooltipInfo] = "Improves Medpacks to 40 instan heal and 10 regen", [kTechDataCostKey] = kTechMed2ResearchCost,             [kTechDataResearchTimeKey] = kTechMed2ResearchTime, }
 	table.insert(techData, medtech1)
 	table.insert(techData, medtech2)
     return techData

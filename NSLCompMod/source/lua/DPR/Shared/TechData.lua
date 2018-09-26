49b7fb46c685f69431297a5019c8795d64953189
Jamescollett <11364579+adsfgg@users.noreply.github.com>
2018-09-26 20:39:14 +0100

    Fixed some formatting stuff and some typos

--- NSLCompMod/source/lua/DPR/Shared/TechData.lua
+++ NSLCompMod/source/lua/DPR/Shared/TechData.lua
@@ -18,7 +18,7 @@ local function NSLTechDataChanges(techData)
             record[kTechDataSupply] = kSentryBatterySupply
 		elseif techDataId == kTechId.Focus then
 			-- focus is now neurotoxin
-			record[kTechDataDisplayName] = "NEUROTOXIN"
+			record[kTechDataDisplayName] = "Neurotoxin"
 			record[kTechDataTooltipInfo] = "Each hit inflicts a poison toxin, hurting marines over time."
 		elseif techToRemove[techDataId] then
 			table.remove(techData, techIndex)
@@ -32,8 +32,22 @@ function BuildTechData()
     local techData = oldBuildTechData()
     NSLTechDataChanges(techData)
 		
-	local medtech1 = { [kTechDataId] = kTechId.MedTech1,       [kTechDataDisplayName] = "Medpack Upgrade #1", [kTechDataTooltipInfo] = "Improves Medpacks to 30 instant heal and 20 regen", [kTechDataCostKey] = kTechMed1ResearchCost,             [kTechDataResearchTimeKey] = kTechMed1ResearchTime, }
-	local medtech2 = { [kTechDataId] = kTechId.MedTech2,       [kTechDataDisplayName] = "Medpack Upgrade #2", [kTechDataTooltipInfo] = "Improves Medpacks to 40 instan heal and 10 regen", [kTechDataCostKey] = kTechMed2ResearchCost,             [kTechDataResearchTimeKey] = kTechMed2ResearchTime, }
+	local medtech1 = { 
+        [kTechDataId] = kTechId.MedTech1,
+        [kTechDataDisplayName] = "Medpack Upgrade #1", 
+        [kTechDataTooltipInfo] = "Improves Medpacks to 30 instant heal and 20 regen", 
+        [kTechDataCostKey] = kTechMed1ResearchCost,             
+        [kTechDataResearchTimeKey] = kTechMed1ResearchTime
+    }
+
+	local medtech2 = { 
+        [kTechDataId] = kTechId.MedTech2,       
+        [kTechDataDisplayName] = "Medpack Upgrade #2", 
+        [kTechDataTooltipInfo] = "Improves Medpacks to 40 instant heal and 10 regen", 
+        [kTechDataCostKey] = kTechMed2ResearchCost,             
+        [kTechDataResearchTimeKey] = kTechMed2ResearchTime
+    }
+
 	table.insert(techData, medtech1)
 	table.insert(techData, medtech2)
     return techData

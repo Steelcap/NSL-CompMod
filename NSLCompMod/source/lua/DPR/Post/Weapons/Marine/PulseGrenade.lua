40875f1883950e8dd99da08b04f7d3d1e10c9db4
adsfgg <11364579+adsfgg@users.noreply.github.com>
2018-10-25 22:36:36 +0100

    Script errorz

--- NSLCompMod/source/lua/DPR/Post/Weapons/Marine/PulseGrenade.lua
+++ NSLCompMod/source/lua/DPR/Post/Weapons/Marine/PulseGrenade.lua
@@ -6,7 +6,7 @@ function PulseGrenade:OnUpdate(deltaTime)
 
     for _, enemy in ipairs( GetEntitiesForTeamWithinRange("Alien", GetEnemyTeamNumber(self:GetTeamNumber()), self:GetOrigin(), kPulseGrenadeAutoExplodeRadius) ) do
     
-        if enemy:GetIsAlive() then
+        if enemy:GetIsAlive() and self.Detonate then
             self:Detonate()
             break
         end

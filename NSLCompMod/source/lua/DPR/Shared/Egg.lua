84aae11e84b0cb4f063580a5d1b1965c181e9b2b
adsfgg <11364579+adsfgg@users.noreply.github.com>
2018-11-08 19:59:20 +0000

    remove spawning with upgrades

--- NSLCompMod/source/lua/DPR/Shared/Egg.lua
+++ NSLCompMod/source/lua/DPR/Shared/Egg.lua
@@ -39,3 +39,59 @@ if Server then
 
 	ReplaceLocals(Egg.OnUse, {GestatePlayer = GestatePlayer})
 end
+
+function Egg:SpawnPlayer(player)
+
+    PROFILE("Egg:SpawnPlayer")
+
+    local queuedPlayer = player
+    
+    if not queuedPlayer or self.queuedPlayerId ~= nil then
+        queuedPlayer = Shared.GetEntity(self.queuedPlayerId)
+    end
+    
+    if queuedPlayer ~= nil then
+    
+        local queuedPlayer = player
+        if not queuedPlayer then
+            queuedPlayer = Shared.GetEntity(self.queuedPlayerId)
+        end
+    
+        -- Spawn player on top of egg
+        local spawnOrigin = Vector(self:GetOrigin())
+        -- Move down to the ground.
+        local _, normal = GetSurfaceAndNormalUnderEntity(self)
+        if normal.y < 1 then
+            spawnOrigin.y = spawnOrigin.y - (self:GetExtents().y / 2) + 1
+        else
+            spawnOrigin.y = spawnOrigin.y - (self:GetExtents().y / 2)
+        end
+
+        local gestationClass = self:GetClassToGestate()
+        
+        -- We must clear out queuedPlayerId BEFORE calling ReplaceRespawnPlayer
+        -- as this will trigger OnEntityChange() which would requeue this player.
+        self.queuedPlayerId = nil
+        
+        local team = queuedPlayer:GetTeam()
+        local success, player = team:ReplaceRespawnPlayer(queuedPlayer, spawnOrigin, queuedPlayer:GetAngles(), gestationClass)                
+        player:SetCameraDistance(0)
+        player:SetHatched()
+        -- It is important that the player was spawned at the spot we specified.
+        assert(player:GetOrigin() == spawnOrigin)
+        
+        if success then
+            -- self:PickUpgrades(player)
+
+            self:TriggerEffects("egg_death")
+            DestroyEntity(self) 
+            
+            return true, player
+            
+        end
+            
+    end
+    
+    return false, nil
+
+end
